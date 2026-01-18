a7332e950da04e8625ed26541a179b4a092309a7
Moritz Pirer <moritz.pirer@student.tugraz.at>
2026-01-18 10:30:59 +0100

    Interpolation Search

---
+++ Searching/InterpolationSearch.py
@@ -0,0 +1,30 @@
+import math
+
+def interpolationSearch(array: list[int], target: int) -> int:
+    left = 0
+    right = len(array) - 1
+
+    while (left <= right and array[left] <= target <= array[right]):
+        if (left == right):
+            return left if array[left] == target else None;
+        
+        # Estimate position
+        position = left + math.floor(
+            ((target - array[left]) * (right - left)) /
+            ((array[right] - array[left]))
+        )
+
+        if (array[position] == target):
+            return position
+        elif (array[position] < target):
+            left = position + 1
+        else:
+            right = position - 1
+
+def main():
+    A = input("Enter array (space seperated): ").split()
+    A = [int(x) for x in A]
+    t = int(input("Enter the target value: "))
+    print(f"Target is at index {interpolationSearch(A, t)}")
+
+main()
---
+++ todo.md
@@ -0,0 +1,32 @@
+# Sorting
+- insertion
+- selection
+- heap
+- radix
+- counting
+- interpolation
+- bubble
+
+# DS
+- Linked list (minimal)
+- stack
+- queue
+- priority queue
+
+# GRAPH
+- Kruskal
+- Prim
+- Kosaraju
+- jarvis wrap
+- graham scan
+- triangulation
+
+# Tree traversal
+- BFS
+- DFS
+- preorder
+- postorder
+- inorder
+
+# DP
+- fibonacci
