8e9350e73ab5df43f62d6a3dc751c70557660ba8
DanGit <daniel.gsteu@hotmail.com>
2026-01-22 01:05:03 +0100

    Implemented further algorithms for super fast sorting - definitely gonna be on exam

--- Sorting/MiracleSort.py
+++ Sorting/MiracleSort.py
@@ -1,14 +1,23 @@
 import time
 
+"""
+Remember the incident in Belgium's 2003 federal election, where the electronic voting machines
+registered 4096 extra votes for one candidate, caused by high-energy particles from space?
+This error was like a miracle (for that one candidate), so it should also be able to sort
+our array - you just have to wait long enough. Let it run, do something else and check on it later.
+Is it sorted yet? No? Well, wait longer you impatient pickle.
+"""
+
 
 def isSorted(list):
     if len(list) == 0:
         print("Miracles cannot come from nothing.")
         exit(-1)
 
-    prev_num = 0
+    prev_num = list[0]
+
     for num in list:
-        if prev_num >= num:
+        if prev_num > num:
             return False
 
         prev_num = num
